665146e38480dd621887209e95ba9b00e13a011b
Rora Alem <rora.goitom@gmail.com>
2023-08-10 10:54:49 -0400

    Update getAllProperties func to use Property listings query

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -127,12 +127,53 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .then((result) => {
-      console.log(result.rows);
-      return result.rows;
-    })
+  const queryParams = [];
+  let queryString = `
+    SELECT properties.*, AVG(property_reviews.rating) AS average_rating
+    FROM properties
+    JOIN property_reviews ON properties.id = property_id
+  `;
+
+  const filterClauses = [];
+// Filter by city if provided
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    filterClauses.push(`properties.city LIKE $${queryParams.length}`);
+  }
+// Filter by minimum_price_per_night if provided
+  if (options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100); // Convert to cents
+    filterClauses.push(`properties.cost_per_night >= $${queryParams.length}`);
+  }
+// Filter by maximum_price_per_night if provided
+  if (options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100); // Convert to cents
+    filterClauses.push(`properties.cost_per_night <= $${queryParams.length}`);
+  }
+// // Filter by minimum_rating if provided
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    filterClauses.push(`property_reviews.rating >= $${queryParams.length}`);
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    filterClauses.push(`properties.owner_id = $${queryParams.length}`);
+  }
+// Add WHERE clause if any filters are provided
+  if (filterClauses.length > 0) {
+    queryString += `WHERE ${filterClauses.join(' AND ')} `;
+  }
+// Add GROUP BY, ORDER BY, and LIMIT clauses
+  queryParams.push(limit);
+  queryString += `
+    GROUP BY properties.id
+    ORDER BY cost_per_night
+    LIMIT $${queryParams.length};
+  `;
+// Execute the query and return results
+  return pool.query(queryString, queryParams)
+    .then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
